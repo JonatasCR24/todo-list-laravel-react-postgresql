1d64933366038a3c50acdcde4ca72f4bb1ee30ce
Jonatas <jcrxl20@gmail.com>
2026-04-18 23:29:21 -0300

    Correção de bug visual

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,20 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title inertia>{{ config('app.name', 'PomoTDL') }}</title>        
 
-        <!-- Fonts -->
+        <title inertia>{{ config('app.name', 'PomoTDL') }}</title>
+
+        <script>
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        </script>
+
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,900&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
         @inertia
     </body>
 </html>
