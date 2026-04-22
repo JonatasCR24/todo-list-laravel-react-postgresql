8cac440e7503d4f0a8a71d7aa9588d9850638781
Jonatas <jcrxl20@gmail.com>
2026-04-21 22:30:13 -0300

    Login via Google

--- routes/web.php
+++ routes/web.php
@@ -72,10 +72,10 @@ Route::middleware('auth')->group(function () {
     // Rotas para visualizar o ranking:
     // (Como é apenas uma rota, mantemos o formato original por simplicidade)
     Route::get('/ranking', [LeaderboardController::class, 'index'])->name('ranking');
-
-    // Rotas do Google
-    Route::get('/auth/google', [GoogleController::class, 'redirect'])->name('google.redirect');
-    Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('google.callback');
 });
+
+// Rotas do Google
+Route::get('/auth/google', [GoogleController::class, 'redirect'])->name('google.redirect');
+Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('google.callback');
 
 require __DIR__ . '/auth.php';
